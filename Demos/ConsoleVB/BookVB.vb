fedb75d7f20411dca96e7f1b569670e19340d699
Walt Ritscher <waltritscher@outlook.com>
2019-05-29 19:06:00 -0700

    Add VB examples

---
+++ Demos/ConsoleVB/BookVB.vb
@@ -0,0 +1,27 @@
+﻿Public Class BookVB
+    Private _title As String
+    Public Property Title() As String
+        Get
+            Return _title
+        End Get
+        Set(ByVal value As String)
+            _title = value
+        End Set
+    End Property
+    Public Sub SaveBook()
+
+    End Sub
+End Class
+
+Public Class Customer
+    Private _customerId As Integer
+    Public Property CustomerID() As Integer
+        Get
+            Return _customerId
+        End Get
+        Set(ByVal value As Integer)
+            _customerId = value
+        End Set
+    End Property
+
+End Class
