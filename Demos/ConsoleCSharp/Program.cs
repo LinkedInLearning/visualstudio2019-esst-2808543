fb95a5e99d759adf82689ecbfaba9b19a91da666
Walt Ritscher <waltritscher@outlook.com>
2019-05-29 18:07:34 -0700

    Add css file, add long comments

--- Demos/ConsoleCSharp/Program.cs
+++ Demos/ConsoleCSharp/Program.cs
@@ -9,6 +9,10 @@ namespace ConsoleCSharp
 			// the canonical sample code for a new program! (HELLO WORLD)
 			Console.WriteLine("Hello World!");
 		}
+
+		private void CommentExample() {
+			// An example of  long, rambling comment that explains why this code is necessary for our core sales application. DO NOT REMOVE the code in this method. Do not change this code without approval. You've been warned. You might think you are improving the code.  You are not! Do not remove. 
+		}
 	}
 
 	// this enum should be declared in a separate file
