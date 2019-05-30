c8c9a2fc864efd9fbb7f858a1eb76c9843fd489e
Walt Ritscher <waltritscher@outlook.com>
2019-05-29 17:18:26 -0700

    Fix enums

--- Demos/ConsoleCSharp/Program.cs
+++ Demos/ConsoleCSharp/Program.cs
@@ -6,7 +6,20 @@ namespace ConsoleCSharp
 	{
 		static void Main(string[] args)
 		{
+			// the canonical sample code for a new program! (HELLO WORLD)
 			Console.WriteLine("Hello World!");
 		}
+	}
+
+	// this enum should be declared in a separate file
+	public enum TourType
+	{
+		None = 0,
+		Walking,
+		Food,
+		Bicycle,
+		Sailing,
+		Cruise
+
 	}
 }
--- Demos/ConsoleVB/Module1.vb
+++ Demos/ConsoleVB/Module1.vb
@@ -4,4 +4,16 @@
 
 	End Sub
 
+
+	' this enum should be declared in a separate file
+	Enum TourType
+		None = 0
+		Walking
+		Food
+		Bicycle
+		Sailing
+		Cruise
+
+	End Enum
+
 End Module
