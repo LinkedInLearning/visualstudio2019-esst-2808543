914ded67dc2f32e7a766db3509989a399e7997a9
Walt Ritscher <waltritscher@outlook.com>
2019-05-29 21:26:48 -0700

    Add example classes

---
+++ Demos/ConsoleCSharp/Document.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCSharp
+{
+    // base class for book and other types
+   public class Document
+    {
+    }
+}
