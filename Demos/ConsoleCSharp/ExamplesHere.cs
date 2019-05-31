5f3220e90131431ecc6481e41b92f68c3a708864
Staff Author <staffauthor@outlook.com>
2019-05-30 22:22:43 -0700

    Rename SE folder

---
+++ Demos/ConsoleCSharp/ExamplesHere.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCSharp
+{
+	class ExamplesHere
+	{
+		// classes contain data and code
+		public string Title { get; set; }
+		public decimal Price { get; set; }
+
+		public void ExampleMethod()
+		{
+			// comments 
+
+			// matching braces
+			if (true)
+			{
+				for (int i = 0; i < 3; i++)
+				{
+					switch (i)
+					{
+						default:
+							break;
+					}
+				}
+			}
+		}
+	}
+}
