91fcf2c28f49d0a3befffc6bd20e6d786d626513
Staff Author <staffauthor@outlook.com>
2019-05-31 19:40:10 -0700

    Add Flags attribute to demo

--- Demos/ConsoleCSharp/Book.cs
+++ Demos/ConsoleCSharp/Book.cs
@@ -5,37 +5,41 @@ using System.Text;
 
 namespace ConsoleCSharp
 {
-  public class Book : Document{
-    // classes contain data and code
-    public string Title { get; set; }
-    public decimal Price { get; set; }
+	public class Book : Document
+	{
+		// classes contain data and code
+		public string Title { get; set; }
+		public decimal Price { get; set; }
 		public Genre Genre { get; set; }
 		public BookInfo Info { get; set; }
 
-		public long GetWordCount() {
-            // comments 
-            return 42;
-    }
-    public void SaveBook() {
-      // comments 
-      
-    }
-  }
+		public long GetWordCount()
+		{
+			// comments 
+			return 42;
+		}
+		public void SaveBook()
+		{
+			// comments 
 
-  public class BookInfo {
-    // structs are primarily for data stores
-    public int PageCount { get; set; }
-    public BookStyle Style { get; set; }
+		}
+	}
 
-    public event EventHandler<EventArgs> SizeChanged;
-  }
+	public class BookInfo
+	{
+		// structs are primarily for data stores
+		public int PageCount { get; set; }
+		public BookStyle Style { get; set; }
 
+		public event EventHandler<EventArgs> SizeChanged;
+	}
 
+	[Flags]
 	public enum BookStyle
 	{
-		Paperback,
-		Hardcover,
-		eBook
+		Paperback = 0,
+		Hardcover = 1,
+		eBook = 2
 
 	}
 	public enum Genre
