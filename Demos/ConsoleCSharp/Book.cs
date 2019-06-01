30ea2775c3b0e8cd623dd49fa417b058cf6c6181
Walt Ritscher <waltritscher@outlook.com>
2019-05-31 21:56:48 -0700

    Add demo HTML and Book content

--- Demos/ConsoleCSharp/Book.cs
+++ Demos/ConsoleCSharp/Book.cs
@@ -3,28 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ConsoleCSharp
+namespace NavigatingCSharp
 {
-  public class Book : Document{
-    // classes contain data and code
-    public string Title { get; set; }
-    public decimal Price { get; set; }
+	class Book
+	{
+		public string BookName { get; set; }
+		public string Author { get; set; }
+		public string ISBN { get; set; }
+		public decimal RoyaltyPercent { get; set; }
 
-    public long GetWordCount() {
-            // comments 
-            return 42;
-    }
-    public void SaveBook() {
-      // comments 
-      
-    }
-  }
+		const Decimal MAX_PRICE = 120;
+		private decimal _price;
+		internal void UpdatePrice(int BookID, decimal newPrice)
+		{
 
-  public struct FileInformation {
-    // structs are primarily for data stores
-    public string FileName { get; set; }
-    public string Size { get; set; }
+			if (newPrice < 0)
+			{
+				_price = newPrice;
+			}
 
-    public event EventHandler<EventArgs> SizeChanged;
-  }
+			// call another method
+			Console.WriteLine("Done");
+			_price = CapPriceAtMaximum(newPrice);
+		}
+
+		/// <summary>
+		///  Cap a book price
+		/// </summary>
+		/// <param name="currentPrice">new book price</param>
+		/// <returns></returns>
+		private decimal CapPriceAtMaximum(decimal currentPrice)
+		{
+			// use the private field
+			_price = 100M;
+
+			if (currentPrice > 120M)
+			{
+				return 120M;
+			}
+			else
+			{
+				return currentPrice;
+			}
+
+
+		}
+	}
 }
