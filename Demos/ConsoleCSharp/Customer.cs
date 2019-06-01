5530d933d6218ad6483eded2f169b82494f91f6c
Staff Author <staffauthor@outlook.com>
2019-05-31 23:49:33 -0700

    Recorded Successfully

---
+++ Demos/ConsoleCSharp/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleCSharp
+{
+	internal class Customer
+	{
+		public Customer()
+		{
+		}
+	}
+}
--- Demos/ConsoleCSharp/ExamplesHere.cs
+++ Demos/ConsoleCSharp/ExamplesHere.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace ConsoleCSharp
 {
-	class ExamplesHere
+	class ExamplesHere :IConvertible
 	{
 
 
@@ -13,12 +12,98 @@ namespace ConsoleCSharp
 
 			// use the Quick Action (Light Bulb)
 
-			var result = Math.Pow(8, 3); // return the cube of 8.
+			_ = Math.Pow(8, 3); // return the cube of 8.
 
-			// var cust = new Customer ();
-			// var serializer = new JsonSerializer();
+			var cust = new Customer();
+		
+			var serializer = new JsonSerializer();
 		}
 
-		
+		public TypeCode GetTypeCode()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool ToBoolean(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public byte ToByte(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public char ToChar(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DateTime ToDateTime(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public decimal ToDecimal(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public double ToDouble(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public short ToInt16(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int ToInt32(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public long ToInt64(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public sbyte ToSByte(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public float ToSingle(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string ToString(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object ToType(Type conversionType, IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ushort ToUInt16(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public uint ToUInt32(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ulong ToUInt64(IFormatProvider provider)
+		{
+			throw new NotImplementedException();
+		}
 	}
+
+	
 }
