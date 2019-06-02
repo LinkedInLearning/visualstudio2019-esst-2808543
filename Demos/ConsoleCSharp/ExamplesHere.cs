4be26f256bf40628fdb481768804d1f8ecae4d3b
Staff Author <staffauthor@outlook.com>
2019-06-02 13:33:59 -0700

    Recorded Successfully

--- Demos/ConsoleCSharp/ExamplesHere.cs
+++ Demos/ConsoleCSharp/ExamplesHere.cs
@@ -9,12 +9,32 @@ namespace ConsoleCSharp
 		// classes contain data and code
 		public string Title { get; set; }
 		public decimal Price { get; set; }
+		public int MyProperty { get; set; }
+		private int myVar;
+
+		public int MyProperty2
+		{
+			get { return myVar; }
+			set { myVar = value; }
+		}
+
+		public ExamplesHere() {
+
+		}
 
 		public void ExampleMethod()
 		{
 
 			// use the Ctrl-K, Ctrl-X combo to see the big list of snippets
+			Console.WriteLine();
+			for (int counter = 0; counter < 10; counter++)
+			{
 
+			}
+			for (int counter = 30 - 1; counter >= 0; counter--)
+			{
+
+			}
 		}
 	}
 }
