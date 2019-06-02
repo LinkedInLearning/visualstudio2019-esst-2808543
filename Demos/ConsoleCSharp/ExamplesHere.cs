e0ae0054aef888eff65a5c1f6ac71a9d7848b9cc
Walt Ritscher <waltritscher@outlook.com>
2019-06-01 20:43:02 -0700

    Remove unused code, change refactoring demos

--- Demos/ConsoleCSharp/ExamplesHere.cs
+++ Demos/ConsoleCSharp/ExamplesHere.cs
@@ -12,20 +12,39 @@ namespace ConsoleCSharp
 
 		public void ExampleMethod()
 		{
-			// comments 
 
-			// matching braces
-			if (true)
-			{
-				for (int i = 0; i < 3; i++)
-				{
-					switch (i)
-					{
-						default:
-							break;
-					}
-				}
-			}
+			var b = new Book();
+			var book = new Book();
+			book.Genre = Genre.Travel;
+			book.Title = "Hiking the highroads in the Himalayas";
+			book.Info.PageCount = 78;
+			book.Info.Style = BookStyle.Paperback;
+
+
+			string filePath = "C:\\demo.txt";
+
+			// refactor the method name
+
+			b.SaveBook(filePath, true, DateTime.Now);
+
+			var b2 = new Book();
+			b2.SaveBook(@"D:\otherfile.txt", false, DateTime.Now);
+
+			// refactor the variable name
+			// refactor the method name
+			// explicit variable
+			// wrap parameters
+			// reorder parameters
+			var p = new PaintLib.Calc();
+
+			var litersProject1 = p.RectanglePaintLiters(coverageRatio: .85, width: 300, height: 140);
+			var litersProject2 = p.EllipsePaintLiters(coverageRatio: .85,
+																										majorAxisLength: 120,
+																										minorAxisLength: 90);
+			//
+			var litersProject3 = p.CirlePaintLiters(radius: 230, coverageRatio: .85);
+			
+			var total = litersProject1 + litersProject2 + litersProject3;
+			Console.WriteLine(total);
 		}
 	}
-}
---
+++ Demos/ConsoleCSharp/Paint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCSharp.PaintLib
+{
+
+	public class Calc
+	{
+		// For real project use  Math.PI 
+		// refactor constant for PI
+		const double areaperratio = 750;
+
+		public double RectanglePaintLiters(double coverageRatio, double width, double height)
+		{
+			var area = width * height;
+			return (area / areaperratio) * coverageRatio;
+		}
+		public double EllipsePaintLiters(double coverageRatio, double majorAxisLength, double minorAxisLength)
+		{
+			var area = 3.141592 * (majorAxisLength * minorAxisLength);
+			return (area / areaperratio) * coverageRatio;
+		}
+
+		public double CirlePaintLiters(double radius, double coverageRatio)
+		{
+			var area = 3.141592 * (radius * radius);
+			return (area / areaperratio) * coverageRatio;
+		}
+	}
+
+}
+
