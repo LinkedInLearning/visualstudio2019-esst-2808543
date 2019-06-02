18a5e274b7b5e95f4590a48e1569a5468a2893d4
Staff Author <staffauthor@outlook.com>
2019-06-01 21:56:29 -0700

    Add more indents

--- Demos/ConsoleCSharp/ExamplesHere.cs
+++ Demos/ConsoleCSharp/ExamplesHere.cs
@@ -11,7 +11,7 @@ namespace ConsoleCSharp
 		public void UseFormatDocument()
 		{
 			// force formatting 
-			int total=       0;
+						int total=       0;
 			int attendeeCount = 4;
 														int result;
 			for (int i = 0; i < 20; i++)
