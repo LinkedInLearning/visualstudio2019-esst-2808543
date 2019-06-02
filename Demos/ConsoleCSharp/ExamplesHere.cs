17668ac2ad73b5bd77454d81742d9b4477377460
Walt Ritscher <waltritscher@outlook.com>
2019-06-02 11:46:28 -0700

    Add codestyle examples

--- Demos/ConsoleCSharp/ExamplesHere.cs
+++ Demos/ConsoleCSharp/ExamplesHere.cs
@@ -1,24 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace ConsoleCSharp
 {
 	class ExamplesHere
 	{
-
+		private int bookCount;
 
 		public void UseFormatDocument()
 		{
-			// force formatting 
-						int total=       0;
-			int attendeeCount = 4;
-														int result;
-			for (int i = 0; i < 20; i++)
+			var bookList = new List<string>();
+			List<string> authorList = new List<string>();
+
+			bool isPremiumCustomer = true;
+
+			// with braces
+			if (isPremiumCustomer)
 			{
-				total += i;
+				Console.WriteLine("true");
 			}
-			result				 = attendeeCount *				total;
+			else
+			{
+				Console.WriteLine("false");
+			}
+
+			// without braces
+			if (isPremiumCustomer)
+				Console.WriteLine("true");
+			else
+				Console.WriteLine("false");
+
+
 		}
 
 		public void DefaultCSharpRules()
