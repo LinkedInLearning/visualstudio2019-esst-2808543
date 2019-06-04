6164ad885b8123f04f56862974aebf706bb782c4
Staff Author <staffauthor@outlook.com>
2019-06-03 19:54:15 -0700

    Clean up code

--- Demos/PublisherLib/Book.cs
+++ Demos/PublisherLib/Book.cs
@@ -3,71 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace PublisherLib {
-	public class Book : Publication {
-    #region Properties
-    public decimal Price { get; set; }
-    public string ISBN { get; set; }
+namespace PublisherLib
+{
+	public class Book : Publication
+	{
+		#region Properties
+		public decimal Price { get; set; }
+		public string ISBN { get; set; }
 		public int CopiesSold { get; set; }
 		#endregion
 
 		const Decimal MAX_PRICE = 120;
 
-    public void UpdatePrice( decimal newPrice) {
-      if (newPrice < 0)
-      {
-        Price = newPrice;
-      }
+		public void UpdatePrice(decimal newPrice)
+		{
+			if (newPrice < 0)
+			{
+				Price = newPrice;
+			}
+			// call another method
+			Console.WriteLine("Done");
+			Price = CapPriceAtMaximum(newPrice);
+		}
+		private decimal CapPriceAtMaximum(decimal currentPrice)
+		{
 
-      // call another method
-      Console.WriteLine("Done");
-      Price = CapPriceAtMaximum(newPrice);
-    }
-    private decimal CapPriceAtMaximum(decimal currentPrice) {
+			// use the private field
+			Price = 100M;
 
+			if (currentPrice > 120M)
+			{
+				return 120M;
+			}
+			else
+			{
+				return currentPrice;
+			}
+		}
+		public decimal CalculateBuyerDiscount(decimal currentPrice, BuyerType buyerType)
+		{
 
+			switch (buyerType)
+			{
+				case BuyerType.Retail:
+					currentPrice = Price;
+					break;
+				case BuyerType.Wholesale:
+					currentPrice = Price * .9M;
+					break;
+				case BuyerType.Academic:
+					currentPrice = Price * .8M;
+					break;
+				case BuyerType.Goverment:
+					currentPrice = Price * .85M;
+					break;
+				default:
+					break;
+			}
+			return currentPrice;
+		}
+	}
 
-      // use the private field
-      Price = 100M;
-
-      if (currentPrice > 120M)
-      {
-        return 120M;
-      }
-      else
-      {
-        return currentPrice;
-      }
-
-
-    }
-    public decimal CalculateBuyerDiscount(decimal currentPrice, BuyerType buyerType) {
-
-      switch (buyerType)
-      {
-        case BuyerType.Retail:
-          currentPrice = Price;
-          break;
-        case BuyerType.Wholesale:
-          currentPrice = Price * .9M;
-          break;
-        case BuyerType.Academic:
-          currentPrice = Price * .8M;
-          break;
-        case BuyerType.Goverment:
-          currentPrice = Price * .85M;
-          break;
-        default:
-          break;
-      }
-      return currentPrice;
-    }
-  }
-
-  public enum BuyerType {
-    Retail,
-    Wholesale,
-    Academic,
-    Goverment
-  }
+	public enum BuyerType
+	{
+		Retail,
+		Wholesale,
+		Academic,
+		Goverment
+	}
 }
